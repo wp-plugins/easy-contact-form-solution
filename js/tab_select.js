2c1aeb89a8a7ff3f8096f4b9e35c80f140df4135
PankajAgarwal <PankajAgarwal@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-12 06:30:25 +0000

    Script File
    git-svn-id: https://plugins.svn.wordpress.org/easy-contact-form-solution/trunk@770798 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/tab_select.js
@@ -0,0 +1,25 @@
+jQuery(document).ready(function() {
+    jQuery('li#goto_fg_advance_form').click(function() {
+        jQuery('#iframebox').css({"display": "block"});
+        jQuery('div.entries_content').css({"display": "none"});
+        jQuery('div.embed_advance_form_div').css({"display": "none"});
+    });
+    jQuery('li#embed_advance_form').click(function() {
+        jQuery('div.embed_advance_form_div').css({"display": "block"});
+        jQuery('div.entries_content').css({"display": "none"});
+        jQuery('#iframebox').css({"display": "none"});
+    });
+    jQuery('li#fgcf_contact_form_content').click(function() {
+        jQuery('div.entries_content').css({"display": "block"});
+        jQuery('#iframebox').css({"display": "none"});
+        jQuery('div.embed_advance_form_div').css({"display": "none"});
+    });
+    jQuery('.delete_entry').click(function(){
+     if (confirm('Are you sure you want to delete this entry')) {
+    var pathname = window.location.pathname;
+     var post_id = jQuery(this).attr("delete_id");
+     var completepath =  pathname + "?page=Fgcf_page&fgcf_del=" + post_id;
+   jQuery("a.delete_entry").attr("href", completepath);
+} 
+    });
+});
