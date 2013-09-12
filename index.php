4e6247c56a3d72f20cf773c7aaf78512b22abb24
PankajAgarwal <PankajAgarwal@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-12 10:42:58 +0000

    Editted index page
    git-svn-id: https://plugins.svn.wordpress.org/easy-contact-form-solution/trunk@770858 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
   Plugin URI: http://www.formget.com
   Description: Extremely simple Contact Form
   Version: 1.0
-  Author: formget
-  Author URI: http://www.forget.com
+  Author: FormGet
+  Author URI: http://www.formget.com
  */
 include_once('fgcf.php');
 include_once('response.php');
@@ -31,22 +31,32 @@ add_action('init', 'fg_scripts');
 register_activation_hook(__FILE__, 'Fgcfc::fgcfc_table_install');
 if (!is_admin()) {
     function fgcfc_show_footer_strip() {
-        ?>	
-        <div class="contac_us_img" style="display:none"><img src="<?php echo plugins_url('/image/contact-button.png', __FILE__); ?>" > </div>
-        <?php
         global $wpdb;
         $fg_iframe_form = get_option('fg_embed_code');
         if (!$fg_iframe_form == null) {
-            $close_image_path = plugins_url('/image/close.png', __FILE__);
-            ?>
+                 $string = " window.onload";
+    $pos = strpos($fg_iframe_form, $string);
+    if ($pos == false) {
+         $close_image_path = plugins_url('/image/close.png', __FILE__);
+        ?>
+        <div class="contac_us_img" style="display:none"><img src="<?php echo plugins_url('/image/contact-button.png', __FILE__); ?>" > </div>
             <div id="fgcf-box-popup" style="display:none">
-                <div id="fg_form_show"> <div class="formget_form"><div id="fg_close" ><img class="close_popup" src="<?php echo $close_image_path; ?>"/></div><?php echo stripslashes($fg_iframe_form); ?></div><p>Powered by <a herf="formget.com">Formget</a> </p></div>
+                <div id="fg_form_show"> <div class="formget_form"><div id="fg_close" ><img class="close_popup" src="<?php echo $close_image_path; ?>"/></div><?php echo stripslashes($fg_iframe_form); ?></div><p>Powered by <a herf="formget.com">FormGet</a> </p></div>
             </div>
             <?php
+    } else {
+       function tabbed_embeded_code() {
+            global $wpdb;
+            $result = get_option('fg_embed_code');
+            echo stripslashes($result);
+        }
+        add_action('wp_head', tabbed_embeded_code);
+    }           
         } else {
             ?>
+				<div class="contac_us_img" style="display:none"><img src="<?php echo plugins_url('/image/contact-button.png', __FILE__); ?>" > </div>
             <div id="fgcf-box-popup"  style="display:none">
-                <div id="fg_form_show"><div class="fgcf_form"> <?php echo Fgcfc::fgcfc_show_form(); ?></div><p id="link_to_formget">Powered by  <a href="http://www.formget.com/" target="_blank"> Formget </a></p></div>
+                <div id="fg_form_show"><div class="fgcf_form"> <?php echo Fgcfc::fgcfc_show_form(); ?></div><p id="link_to_formget">Powered by  <a href="http://www.formget.com/" target="_blank"> FormGet </a></p></div>
             </div>
             <?php
         }
